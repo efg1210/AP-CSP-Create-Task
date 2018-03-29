7ff05f6f5d4ab4e786f5a8db04fe88ced0daa06c
Michael Moore <mmoore3@ucvts.org>
2018-03-29 15:14:09 +0000

    More starting edits

--- js/js.js
+++ js/js.js
@@ -1,4 +1,22 @@
-function startFunction(){
+function entered(e){
+    e.preventDefault();
+    var input = e.target.userInputTwo.value;
+    //console.log(e.target.userInputTwo.value);
+    console.log(input);
+    document.getElementById("userInputTwo").value = "> ";
+    var choice = input.substr(2);
+    console.log(choice);
+}
 
-    
+function firstChars() {
+    var lol = document.getElementById("userInputTwo").value;
+    var arr = lol.split('');
+    if(arr[1] != " ") {
+        arr = arr.reverse();
+            arr.pop(arr.length - 1); 
+            arr.push(" ", ">");
+        arr = arr.reverse();
+        lol = arr.join("");
+        document.getElementById("userInputTwo").value = lol;
+    }
 }
