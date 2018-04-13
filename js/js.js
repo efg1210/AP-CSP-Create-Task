c87cfa548ec9dcaa558ac343e002b7a7025f0993
Michael Moore <mmoore3@ucvts.org>
2018-04-13 23:09:45 +0000

    Editing everything, it's possible nothing works

--- js/js.js
+++ js/js.js
@@ -1,22 +1,64 @@
+
+//var that will NOT reset after each user input, so it should be global
+var possessions = [];
+var beachOptions = ["go into the ocean for a nice swim", "explore the caves", "look around the beach for shells"];
+
+//turns what the user inputs into usable data
 function entered(e){
     e.preventDefault();
-    var input = e.target.userInputTwo.value;
-    //console.log(e.target.userInputTwo.value);
+    var input = e.target.userInput.value;
     console.log(input);
-    document.getElementById("userInputTwo").value = "> ";
+    document.getElementById("userInput").value = "> ";
     var choice = input.substr(2);
     console.log(choice);
+    displayChoice(choice);
+    displayResult(choice);
 }
 
+//makes the static > and space before user input
 function firstChars() {
-    var lol = document.getElementById("userInputTwo").value;
-    var arr = lol.split('');
+    var value = document.getElementById("userInput").value;
+    var arr = value.split('');
     if(arr[1] != " ") {
         arr = arr.reverse();
             arr.pop(arr.length - 1); 
             arr.push(" ", ">");
         arr = arr.reverse();
-        lol = arr.join("");
-        document.getElementById("userInputTwo").value = lol;
+        value = arr.join("");
+        document.getElementById("userInput").value = value;
     }
+}
+
+function story(choice){
+    var result;
+    if(choice == "go into the ocean for a nice swim"){
+        if(possession.include("necklace") || possession.include("bracelet")){
+            
+        }
+    }else if(choice == "explore the caves"){
+        document.getElementById().innerHTML += choice;
+    }else if(choice == "look around the beach for shells"){
+        
+    }else if(choice == ""){
+        
+    }
+    return result;
+}
+
+//shows the choice the user made
+function displayChoice(choice) {
+    document.getElementById("textarea").innerHTML += choice + "<br>";
+    story(choice);
+}
+
+//displays the result of the choice
+function displayResult(result) {
+    var div = document.getElementById("textarea");
+    var display = result + div;
+    div.innerHTML += display + "<br>";
+}
+
+//
+function storyOne() {
+    displayResult("");
 }
